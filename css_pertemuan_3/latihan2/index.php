d12dba1adcba1dde1f29e2b1cc1889a5342c66f5
maubi <miftahulmaubi32@gmail.com>
2024-05-06 15:13:01 +0700

    latihan

---
+++ css_pertemuan_3/latihan2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar CSS Dasar</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class ="container">
+        <div class= "card">
+            <div class="card-img">
+                <img src="img.jpg" alt="">
+            </div>
+
+            <div class="card-body">
+                <h2 class="card-title">Higanbana</h2>
+                <p class="card-intro">Higanbana (彼岸花) is the Japanese name for a flower also known as “Spider Lily” or “Lycoris Radiata” in English.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
